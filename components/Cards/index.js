f9f7d1aa008052d44873c1fd05ea9076ee95b0c5
MaryamMosstoufi <maryam@zooxoos.com>
2020-06-05 11:45:29 -0400

    Cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,50 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+
+        const articlesArray = Object.entries(response.data.articles);
+
+        for (let i = 0; i < articlesArray.length; i++) {
+            // let articleTopic = articlesArray[i][0];
+
+            for (let j = 0; j < articlesArray[i][1].length; j++) {
+                let newCard = cardMaker(articlesArray[i][1][j]);
+                document.querySelector('.cards-container').appendChild(newCard);
+                // console.log(articlesArray[i][1][j]);
+            }
+        }
+    })
+    .catch((error) => {
+        console.log("Error:", error);
+    })
+
+function cardMaker(cardData) { // the cardData will be response.data.articles.(each-topic)[i]
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+
+    const headlineDiv = document.createElement('div');
+    headlineDiv.classList.add('headline');
+    headlineDiv.textContent = cardData.headline;
+    cardDiv.appendChild(headlineDiv);
+
+    const authorDiv = document.createElement('div');
+    authorDiv.classList.add('author');
+    cardDiv.appendChild(authorDiv);
+
+    const imgDiv = document.createElement('div');
+    imgDiv.classList.add('img-container');
+    authorDiv.appendChild(imgDiv);
+
+    const imgElement = document.createElement('img');
+    imgElement.setAttribute('src', cardData.authorPhoto);
+    imgDiv.appendChild(imgElement);
+
+    const authorNameSpan = document.createElement('span');
+    authorNameSpan.textContent = cardData.authorName;
+    authorDiv.appendChild(authorNameSpan);
+
+    return cardDiv;
+}
